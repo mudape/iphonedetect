e057e3a7338177bc1b96392f0b0f108808e10968
Magnus Nyström <github@mudape.com>
2021-03-05 19:20:15 +0100

    Update update_manifest.py
    Copy, paste.. Forgot to change path

--- manage/update_manifest.py
+++ manage/update_manifest.py
@@ -11,13 +11,13 @@ def update_manifest():
         if value in ["--version", "-V"]:
             version = sys.argv[index + 1]
 
-    with open(f"{os.getcwd()}/custom_components/hacs/manifest.json") as manifestfile:
+    with open(f"{os.getcwd()}/custom_components/iphonedetect/manifest.json") as manifestfile:
         manifest = json.load(manifestfile)
 
     manifest["version"] = version
 
     with open(
-        f"{os.getcwd()}/custom_components/hacs/manifest.json", "w"
+        f"{os.getcwd()}/custom_components/iphonedetect/manifest.json", "w"
     ) as manifestfile:
         manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))
 
