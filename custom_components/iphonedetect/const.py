74dcdd2ece7d7b583bdf20f4b3df5830d2aa7ef1
Magnus Nyström <github@mudape.com>
2022-04-14 22:57:14 +0200

    Update const.py

--- custom_components/iphonedetect/const.py
+++ custom_components/iphonedetect/const.py
@@ -1,8 +1,28 @@
-HOME_STATES={
-    2: "REACHABLE",
-    8: "DELAY",
-    # 4: "STALE",
-} 
+"""Constants for the iPhone Device Tracker integration."""
 
-CONST_MESSAGE = b"Marco"
-CONST_MESSAGE_PORT = 5353
+from homeassistant.const import Platform
+
+NAME = "iPhone Device Tracker"
+DOMAIN = "iphonedetect"
+
+PLATFORMS = [
+    Platform.DEVICE_TRACKER,
+]
+
+DEFAULT_SCAN_INTERVAL: int = 12
+
+DEFAULT_CONSIDER_HOME: int = 30
+MIN_CONSIDER_HOME: int = 20
+MAX_CONSIDER_HOME: int = 120
+
+CONF_NUD_STATE = {
+    0: {"state": "None", "home": False},
+    1: {"state": "Incomplete", "home": False},
+    2: {"state": "Reachable", "home": True},
+    4: {"state": "Stale", "home": True},
+    8: {"state": "Delay", "home": True},
+    16: {"state": "Probe", "home": False},
+    32: {"state": "Failed", "home": False},
+    64: {"state": "Noarp", "home": False},
+    128: {"state": "Permanent", "home": True},
+}
