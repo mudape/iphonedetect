38f16eb975c90f6f0f4903083a4ad3a1f84cd3f6
Giel Janssens <gieljnssns@me.com>
2023-02-17 02:54:47 +0100

    Update __init__.py
    Should solve https://github.com/mudape/iphonedetect/issues/89

--- custom_components/iphonedetect/__init__.py
+++ custom_components/iphonedetect/__init__.py
@@ -83,7 +83,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = entry.entry_id
 
     entry.async_on_unload(entry.add_update_listener(async_update_options))
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
