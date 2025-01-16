d1a398b5f3ba44b1d0152295c5259e1f4912c7e1
mudape <github@mudape.com>
2025-01-16 16:03:02 +0100

    Adjust config flow

--- custom_components/iphonedetect/config_flow.py
+++ custom_components/iphonedetect/config_flow.py
@@ -14,13 +14,16 @@ from homeassistant.config_entries import (
     ConfigEntry,
     ConfigFlow,
     FlowResult,
-    OptionsFlow,
 )
 from homeassistant.const import (
     CONF_IP_ADDRESS,
     CONF_NAME,
 )
 from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.schema_config_entry_flow import (
+    SchemaFlowFormStep,
+    SchemaOptionsFlowHandler,
+)
 from homeassistant.util import slugify
 
 from .const import (
@@ -30,28 +33,31 @@ from .const import (
     MIN_CONSIDER_HOME,
 )
 
+
 _LOGGER = logging.getLogger(__name__)
 
 
-async def validate_ip_address(subnets: list[IPv4Network], devices: list, ip: str) -> dict:
-    """Try to validate user input"""
-    errors = {}
+OPTIONS_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_CONSIDER_HOME, default=DEFAULT_CONSIDER_HOME): vol.All(
+            vol.Coerce(int),
+            vol.Range(min=MIN_CONSIDER_HOME, max=MAX_CONSIDER_HOME),
+        )
+    }
+)
 
-    if ip in devices:
-        errors["base"] = "ip_already_configured"
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME, description={"suggested_value": "My iPhone"}): str,
+        vol.Required(CONF_IP_ADDRESS, description={"suggested_value": "192.168.1.xx"}): str,
+        **OPTIONS_SCHEMA.schema,
+        vol.Optional("subnet_check", default=True): bool,
+    }
+)
 
-    if not errors:
-        try:
-            ip_address = IPv4Address(ip)
-        except AddressValueError:
-            errors["base"] = "ip_invalid"
-            return errors
-
-    if not errors:
-        if not any(ip_address in subnet for subnet in subnets):
-            errors["base"] = "ip_range"
-
-    return errors
+OPTIONS_FLOW = {
+    "init": SchemaFlowFormStep(OPTIONS_SCHEMA),
+}
 
 
 async def async_get_networks(hass: HomeAssistant) -> list[IPv4Network]:
@@ -68,36 +74,34 @@ async def async_get_networks(hass: HomeAssistant) -> list[IPv4Network]:
     return networks
 
 
-class IphoneDetectOptionsFlowHandler(OptionsFlow):
-    """iPhone Detect config flow options handler."""
+async def _validate_input(hass: HomeAssistant, user_input: dict[str, Any]) -> dict[str, str] | None:
+    """Try to validate user input"""
+    entries = [entry for entry in hass.config_entries.async_entries(DOMAIN)]
+    ip = user_input[CONF_IP_ADDRESS]
 
-    async def async_step_init(self, user_input: dict[str, Any] | None = None) -> FlowResult:
-        """Manage the options."""
-        return await self.async_step_user(user_input)
+    # Check if name already used for a clearer error
+    if user_input.get(CONF_NAME, False):
+        entries_id = [entry.unique_id for entry in entries]
+        entry_id = f"{DOMAIN}_{slugify(user_input[CONF_NAME]).lower()}"
+        if entry_id in entries_id:
+            return {"base": "name_not_unique"}
 
-    async def async_step_user(self, user_input: dict[str, Any] | None = None) -> FlowResult:
-        """Handle options flow."""
-        errors = {}
+    # Check if valid IP address
+    try:
+        IPv4Address(ip)
+    except AddressValueError:
+        return {"base": "ip_invalid"}
 
-        if user_input is not None and user_input[CONF_CONSIDER_HOME] != self.config_entry.options[CONF_CONSIDER_HOME]:
-            new_options = self.config_entry.options | {CONF_CONSIDER_HOME: user_input[CONF_CONSIDER_HOME]}
-            return self.async_create_entry(title="", data=new_options)
+    # Check if IP address already used for a clearer error
+    entries_ip = [entry.options[CONF_IP_ADDRESS] for entry in entries]
+    if ip in entries_ip:
+        return {"base": "ip_already_configured"}
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_CONSIDER_HOME,
-                        default=self.config_entry.options[CONF_CONSIDER_HOME],
-                    ): vol.All(
-                        vol.Coerce(int),
-                        vol.Range(min=MIN_CONSIDER_HOME, max=MAX_CONSIDER_HOME),
-                    ),
-                }
-            ),
-            errors=errors,
-        )
+    # Check if device IP will be seen by ARP
+    if user_input["subnet_check"]:
+        subnets = await async_get_networks(hass)
+        if not any(IPv4Address(ip) in subnet for subnet in subnets):
+            return {"base": "ip_range"}
 
 
 class IphoneDetectFlowHandler(ConfigFlow, domain=DOMAIN):  # type: ignore
@@ -105,16 +109,17 @@ class IphoneDetectFlowHandler(ConfigFlow, domain=DOMAIN):  # type: ignore
 
     VERSION = 2
 
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: ConfigEntry) -> SchemaOptionsFlowHandler:
+        return SchemaOptionsFlowHandler(config_entry, OPTIONS_FLOW)
+
     async def async_step_user(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Handle a flow initialized by the user."""
         errors = {}
 
         if user_input is not None:
-            ip = user_input[CONF_IP_ADDRESS]
-            devices = [dev.options[CONF_IP_ADDRESS] for dev in self.hass.config_entries.async_entries(DOMAIN)]
-            subnet = await async_get_networks(self.hass)
-
-            errors = await validate_ip_address(subnet, devices, ip)
+            errors = await _validate_input(self.hass, user_input)
 
             if not errors:
                 unique_id = slugify(user_input[CONF_NAME]).lower()
@@ -122,27 +127,19 @@ class IphoneDetectFlowHandler(ConfigFlow, domain=DOMAIN):  # type: ignore
                 self._abort_if_unique_id_configured()
 
                 self._async_abort_entries_match({CONF_NAME: user_input[CONF_NAME]})
+
                 return self.async_create_entry(
                     title=user_input[CONF_NAME],
                     data={},
                     options={
-                        CONF_IP_ADDRESS: ip,
+                        CONF_IP_ADDRESS: user_input[CONF_IP_ADDRESS],
                         CONF_CONSIDER_HOME: user_input[CONF_CONSIDER_HOME],
                     },
                 )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_NAME, description={"suggested_value": "My iPhone"}): str,
-                    vol.Required(CONF_IP_ADDRESS, description={"suggested_value": "192.168.1.xx"}): str,
-                    vol.Required(CONF_CONSIDER_HOME, default=DEFAULT_CONSIDER_HOME): vol.All(
-                        vol.Coerce(int),
-                        vol.Range(min=MIN_CONSIDER_HOME, max=MAX_CONSIDER_HOME),
-                    ),
-                }
-            ),
+            data_schema=self.add_suggested_values_to_schema(DATA_SCHEMA, user_input),
             errors=errors,
         )
 
@@ -167,38 +164,24 @@ class IphoneDetectFlowHandler(ConfigFlow, domain=DOMAIN):  # type: ignore
 
     async def async_step_reconfigure(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Handle options flow."""
-        entry = self.hass.config_entries.async_get_entry(self.context["entry_id"])
-        assert entry
-
         errors = {}
+        entry = self._get_reconfigure_entry()
+
         if user_input is not None:
-            new_ip = user_input.get(CONF_IP_ADDRESS)
-            current_ip = entry.options[CONF_IP_ADDRESS]
+            errors = await _validate_input(self.hass, user_input)
+            if not errors:
+                await self.async_set_unique_id(entry.unique_id)
+                self._abort_if_unique_id_mismatch()
+                new_options = entry.options | {CONF_IP_ADDRESS: user_input[CONF_IP_ADDRESS]}
 
-            if new_ip and new_ip != current_ip:
-                devices = [other_entry.options[CONF_IP_ADDRESS] for other_entry in self.hass.config_entries.async_entries(DOMAIN) if other_entry.entry_id != entry.entry_id]
-                subnets = await async_get_networks(self.hass)
-
-                errors = await validate_ip_address(subnets, devices, new_ip)
-
-                if not errors:
-                    new_options = entry.options | {
-                        CONF_IP_ADDRESS: new_ip,
-                    }
-                    return self.async_update_reload_and_abort(entry, options=new_options, reason="reconfigure_successful")
+                return self.async_update_reload_and_abort(
+                    entry,
+                    options=new_options,
+                )
 
         return self.async_show_form(
             step_id="reconfigure",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_IP_ADDRESS, default=entry.options[CONF_IP_ADDRESS]): str,
-                }
-            ),
+            data_schema=vol.Schema({vol.Required(CONF_IP_ADDRESS, default=entry.options[CONF_IP_ADDRESS]): str, vol.Optional("subnet_check", default=True): bool}),
             description_placeholders={"device_name": entry.title},
             errors=errors,
         )
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry: ConfigEntry) -> IphoneDetectOptionsFlowHandler:
-        return IphoneDetectOptionsFlowHandler()
